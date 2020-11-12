f8d4276a7fee001c1df3b791c4431cb48e9275b9
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-11-12 14:53:50 -0500

    blank starter project w/ bootstrap

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular is too kewl for skewl';
-  name = 'Kevin'
+  title = "Kevin and Zuma";
 }
